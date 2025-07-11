17735c71b3d489721319ba39312064c6a39d37fe
Chidi Mathaias <chidimathayas@gmail.com>
2025-07-11 23:05:19 +0100

    feat: enhance WelcomeMessage component with dynamic greeting and date display

--- src/components/section/dashboard/WelcomeMessage.jsx
+++ src/components/section/dashboard/WelcomeMessage.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const WelcomeMessage = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setDate(new Date());
+  }, []);
+
+  const formattedDate = date.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const greeting = () => {
+    const hour = date.getHours();
+    if (hour < 12) return "Good morning";
+    if (hour < 17) return "Good afternoon";
+    return "Good evening";
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-8 py-10">
       <h2 className="text-6xl font-bold text-green-700 normal-case">
-        Good morning, Alex!
+        {greeting()}, Alex!
       </h2>
       <p className="text-2xl font-semibold text-gray-700 normal-case">
-        Friday, July 11, 2025
+        {formattedDate}
       </p>
       <p className="text-2xl font-medium text-blue-700 normal-case">
         Let's check how you're doing today
